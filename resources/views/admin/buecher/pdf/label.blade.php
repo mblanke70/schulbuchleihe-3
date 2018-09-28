44401e5da015284dce79da353cda066cdf8bb9ab
mblanke70 <blanke@ursulaschule.de>
2018-09-28 20:29:38 +0200

    label...

--- resources/views/admin/buecher/pdf/label.blade.php
+++ resources/views/admin/buecher/pdf/label.blade.php
@@ -13,12 +13,12 @@
 		<div style="position:absolute; left: 0px; top: 40px;">{{ $buch->aufnahmedatum }}</div>
 		<div style="position:absolute; right: 0px; top: 0px; text-align: center;">
 			<img src="data:image/png;base64,{{ DNS1D::getBarcodePNG("42305", "C39", 2, 55) }}" alt="barcode"/><br />
-			<span style="fon-size:20px;">{{ $buch->id }}</span>
+			<span style="font-size:20px;">{{ $buch->id }}</span>
 		</div>
-		<div style="position:absolute; left: 0px; top: 80px;">
+		<div style="position:absolute; left: 0px; top: 75px;">
 			<p style="font-size:18px;">{{ htmlentities($buch->buchtitel->titel) }}</p>
 		</div>
-		<div style="position:absolute; left: 0px; top: 105px;">
+		<div style="position:absolute; left: 0px; top: 100px;">
 			<p style="font-size:16px;">Ursulaschule Osnabr&uuml;ck</p>
 			<p style="font-size:12px;">Dieses Buch ist Eigentum des Bistums Osnabr&uuml;ck. Dieses Buch ist pfleglich zu behandeln. Eintragungen, Randbemerkungen u.a. d&uuml;rfen nicht vorgenommen werden. Bei nicht fristgem&auml;&szlig;er R&uuml;ckgabe oder Besch&auml;digung wird die Schule Schadenersatz verlangen.</p>
 		</div>
