8867b9ce90a9172eb2da4b0e12062f626bcbcc44
mblanke70 <blanke@ursulaschule.de>
2019-02-16 16:58:44 +0100

    ...

--- resources/views/admin/buecher/pdf/label.blade.php
+++ resources/views/admin/buecher/pdf/label.blade.php
@@ -12,16 +12,16 @@
 		</div>
 		<div style="position: absolute; left: 0px; top: 40px;">{{ $buch->aufnahmedatum }}</div>
 		<div style="position: absolute; right: 0px; top: 0px; text-align: center;">
-			<img src="data:image/png;base64,{{ DNS1D::getBarcodePNG("42305", "C39", 2, 55) }}" alt="barcode"/><br />
+			<img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($buch->id, "C39", 2, 55) }}" alt="barcode"/><br />
 			<span style="font-size:20px;">{{ $buch->id }}</span>
 		</div>
-		<div style="position: absolute; left: 0px; top: 75px;">
+		<div style="position: absolute; left: 0px; top: 70px;">
 			<p style="font-size:18px;">{{ htmlentities($buch->buchtitel->titel) }}</p>
 		</div>
-		<div style="position: absolute; left: 0px; top: 100px;">
+		<div style="position: absolute; left: 0px; top: 95px;">
 			<p style="font-size:16px;">Ursulaschule Osnabr&uuml;ck</p>
 		</div>
-		<div style="position: absolute; left: 0px; top: 125px;">
+		<div style="position: absolute; left: 0px; top: 120px;">
 			<p style="font-size:12px;">Dieses Buch ist Eigentum des Bistums Osnabr&uuml;ck. Dieses Buch ist pfleglich zu behandeln. Eintragungen, Randbemerkungen u.a. d&uuml;rfen nicht vorgenommen werden. Bei nicht fristgem&auml;&szlig;er R&uuml;ckgabe oder Besch&auml;digung wird die Schule Schadenersatz verlangen.</p>
 		</div>
 	</div>	
