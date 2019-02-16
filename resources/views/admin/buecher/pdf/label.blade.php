01f0890ec5a64e851453cd05aa81f67ea7e5baef
mblanke70 <blanke@ursulaschule.de>
2019-02-16 17:02:51 +0100

    ...

--- resources/views/admin/buecher/pdf/label.blade.php
+++ resources/views/admin/buecher/pdf/label.blade.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>`
 	<title>Label #{{$buch->id}}</title>
 	<style type="text/css">
 		*  {padding: 0; margin: 0; font-family: Arial, sans-serif;}
