35123e6564d486eaf5200789f218c15c87c25a62
mblanke70 <blanke@ursulaschule.de>
2019-04-24 23:24:30 +0200

    ...

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -64,8 +64,8 @@ class LoginController extends Controller
      */
     public function handleProviderCallback()
     {
-        $iservUser = Socialite::driver('iserv')->stateless()->user();
-        
+        $iservUser = Socialite::driver('iserv')->user();
+
         $user = User::where( 'email', $iservUser->email )->first();
         
         /*
