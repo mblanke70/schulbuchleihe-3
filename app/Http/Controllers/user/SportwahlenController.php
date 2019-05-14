9769399e4cdc7e2f9ef646c17e23f593b52e0478
mblanke70 <blanke@ursulaschule.de>
2019-05-14 21:20:40 +0200

    ...

--- app/Http/Controllers/user/SportwahlenController.php
+++ app/Http/Controllers/user/SportwahlenController.php
@@ -29,7 +29,7 @@ class SportwahlenController extends Controller
 
 	    if( $sportwahl == null )
 	    {
-	    	return redirect('sportwahlen/wahlbogen');
+	    	return redirect('user/sportwahlen/wahlbogen');
 	    }
 	    else
 	    {
