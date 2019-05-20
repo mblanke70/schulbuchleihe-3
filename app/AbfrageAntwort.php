f5abc919c32e9b5a935285be15a39a9db2280577
mblanke70 <blanke@ursulaschule.de>
2019-05-20 17:36:08 +0200

    ...

--- app/AbfrageAntwort.php
+++ app/AbfrageAntwort.php
@@ -18,7 +18,7 @@ class AbfrageAntwort extends Model
      */
     public function abfrage()
     {
-    	return $this->belongsTo('App\Abfrage', 'antwort_id', 'id');
+    	return $this->belongsTo('App\Abfrage');
     }
 
     /**
