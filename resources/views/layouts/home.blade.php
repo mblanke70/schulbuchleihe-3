0c48f7b398eb588ff23fe33d42b29db7b009d11c
mblanke70 <blanke@ursulaschule.de>
2019-06-03 23:21:05 +0200

    ...

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -34,7 +34,7 @@
   </head>
   <body>
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Ursulaschule Osnabrück</a>
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ url('user/') }}">Ursulaschule Osnabrück</a>
  
 </nav>
 
