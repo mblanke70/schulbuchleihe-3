49a0db806daefb3c8e112288705a0ee7f9fe7365
mblanke70 <blanke@ursulaschule.de>
2019-06-04 19:44:55 +0200

    ...

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -45,8 +45,9 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return $user->istAdmin();
-        });
+            return in_array($user->email, [
+            'malte.blanke@urs-os.de',
+        ]);
     }
 
     /**
