340756ea23770fd35b6ec4ca2a03725d1f8912a2
mblanke70 <blanke@ursulaschule.de>
2019-07-05 17:10:30 +0200

    ...

--- resources/views/pdf/rechnung.blade.php
+++ resources/views/pdf/rechnung.blade.php
@@ -439,10 +439,6 @@
           @php ($jahr = date_format($buch->aufnahme, 'Y'))    
           @php ($restwert = number_format((ceil($btsj->kaufpreis) - (2019 - $jahr) * $btsj->leihpreis), 2)) 
 
-          @php
-            if($restwert<0) $restwert = 0;
-          @endphp
-
           <tr data-iterate="item">
             <td>{{ $i++ }}</td>
             <td>{{ $buch->buchtitel->titel }}</td>
