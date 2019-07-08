45d55cd289e9457718519bbdf485758cf005122e
mblanke70 <blanke@ursulaschule.de>
2019-07-08 22:30:28 +0200

    ...

--- resources/views/pdf/rechnung.blade.php
+++ resources/views/pdf/rechnung.blade.php
@@ -201,7 +201,7 @@
 
         #invoice-info {
           float: right;
-          margin-top: 60px;
+          margin-top: 50px;
         }
         #invoice-info > div {
           float: left;
@@ -226,7 +226,7 @@
         }
 
         #items {
-          margin-top: 50px;
+          margin-top: 60px;
         }
         #items .first-cell, #items table th:first-child, #items table td:first-child {
           width: 10px;
