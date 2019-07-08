b7574d76c4e0dc03e3fd9a64c4e7f367d3f6d6dc
mblanke70 <blanke@ursulaschule.de>
2019-07-08 22:29:25 +0200

    ...

--- resources/views/pdf/rechnung.blade.php
+++ resources/views/pdf/rechnung.blade.php
@@ -226,7 +226,7 @@
         }
 
         #items {
-          margin-top: 60px;
+          margin-top: 50px;
         }
         #items .first-cell, #items table th:first-child, #items table td:first-child {
           width: 10px;
