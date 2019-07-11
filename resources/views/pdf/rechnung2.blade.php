dc6886bd277e9b4c7b55460f599b3229e9621a56
mblanke70 <blanke@ursulaschule.de>
2019-07-11 10:32:26 +0200

    ...

--- resources/views/pdf/rechnung2.blade.php
+++ resources/views/pdf/rechnung2.blade.php
@@ -425,8 +425,8 @@
         </div>
         
         <div>
-          <span>{{ date_format($rechnung->re_datum, 'd.m.Y') }}</span>
-          <span>{{ date_format($rechnung->re_faelligkeit, 'd.m.Y') }}</span>
+          <span>{{ date('d.m.Y', strtotime($rechnung->re_datum)) }}</span>
+          <span>{{ date('d.m.Y', strtotime($rechnung->re_faelligkeit)) }}</span>
           <span>{{ $rechnung->s_schuljahr }}</span>
           <span>{{ $rechnung->s_vorname . ' ' . $rechnung->s_nachname }}</span>
           <span>2019-{{ str_pad($rechnung->id, 3, '0', STR_PAD_LEFT) }}</span>
@@ -504,7 +504,7 @@
           <strong>{{ $rechnung->s_vorname }}</strong> sind die oben aufgeführten Lernmittel leihweise überlassen worden. Diese wurden nicht bzw. beschädigt zurückgegeben, so dass eine weitere Ausleihe nicht möglich ist. Nach den von Ihnen anerkannten Ausleihbedingungen sind Sie verplichtet, den Zeitwert der Lernmittel erstatten. (Lernmittel, deren Zeitwert 3,00&nbsp;€ oder weniger beträgt, sind nicht brücksichtigt worden.)
         </div>
         
-        <div>Ich bitte Sie deshalb um die Überweisung des Betrages von&nbsp;&nbsp;<strong>{{ number_format($posten->re_summe, 2, ',', ' ') }} €</strong>&nbsp;&nbsp;bis zum <strong>{{ date_format($rechnung->re_faelligkeit, 'd.m.Y') }}</strong> auf das unten angegebene Konto. Im Betreff der Überweisung bitte unbedingt die Rechnungsnummer angeben.</div>
+        <div>Ich bitte Sie deshalb um die Überweisung des Betrages von&nbsp;&nbsp;<strong>{{ number_format($posten->re_summe, 2, ',', ' ') }} €</strong>&nbsp;&nbsp;bis zum <strong>{{ date('d.m.Y', strtotime($rechnung->re_faelligkeit)) }}</strong> auf das unten angegebene Konto. Im Betreff der Überweisung bitte unbedingt die Rechnungsnummer angeben.</div>
 
         <div>
         Wenn 
