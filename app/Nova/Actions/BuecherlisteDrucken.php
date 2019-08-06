59ed69609a0a7a406ba9614622adf594819304f9
mblanke70 <blanke@ursulaschule.de>
2019-08-06 13:38:14 +0200

    ...

--- app/Nova/Actions/BuecherlisteDrucken.php
+++ app/Nova/Actions/BuecherlisteDrucken.php
@@ -35,37 +35,45 @@ class BuecherlisteDrucken extends Action
      */
     public function handle(ActionFields $fields, Collection $models)
     {        
-        $schueler = $models->first();
+        $liste = collect();
 
-        // Hole alle Buchtitel, die auf der Bücherliste des Jahrgangs des Schülers stehen       
-        $buchtitel     = $schueler->klasse->jahrgang->buchtitel;
-        // Hole alle Bücher, die der Schüler derzeit ausgeliehen hat
-        $buecher       = $schueler->buecher;
-        // Hole alle Buchbestellungen, die der Schüler abgegeben hat
-        $buecherwahlen = $schueler->buecherwahlen->keyBy('buchtitel_id');
-        
-        // Durchlaufe die Bücherliste und ergänze zu jedem Buchtitel
-        //   - die zugehörige Bestellung
-        //   - den aktuellen Leihstatus (ist der Buchtitel bereits als Buch ausgeliehen worden?) 
-        foreach($buchtitel as $btsj) 
-        {    
-            // bestellt?
-            //$bw = $buecherwahlen->get($bt->buchtitel_id);
-            $bw = $buecherwahlen->get($btsj->id);
-            if($bw!=null) {
-                $btsj['wahl']    = $bw->wahl;
-                $btsj['wahl_id'] = $bw->id;
-            } else {
-                $btsj['wahl'] = 4;    // == nicht bestellt (abgewählt)
+        foreach($models as $schueler) 
+        {
+            // Hole alle Buchtitel, die auf der Bücherliste des Jahrgangs des Schülers stehen       
+            $buchtitel     = $schueler->klasse->jahrgang->buchtitel;
+            // Hole alle Bücher, die der Schüler derzeit ausgeliehen hat
+            $buecher       = $schueler->buecher;
+            // Hole alle Buchbestellungen, die der Schüler abgegeben hat
+            $buecherwahlen = $schueler->buecherwahlen->keyBy('buchtitel_id');
+            
+            // Durchlaufe die Bücherliste und ergänze zu jedem Buchtitel
+            //   - die zugehörige Bestellung
+            //   - den aktuellen Leihstatus (ist der Buchtitel bereits als Buch ausgeliehen worden?) 
+            foreach($buchtitel as $btsj) 
+            {    
+                // bestellt?
+                $bw = $buecherwahlen->get($btsj->id);
+                if($bw!=null) {
+                    $btsj['wahl']    = $bw->wahl;
+                    $btsj['wahl_id'] = $bw->id;
+                } else {
+                    $btsj['wahl'] = 4;    // == nicht bestellt (abgewählt)
+                }
+
+                // ausgeliehen?
+                $btsj['ausgeliehen'] = $buecher->contains('buchtitel_id', $btsj->buchtitel->id) ? 1 : 0;
             }
 
-            // ausgeliehen?
-            $btsj['ausgeliehen'] = $buecher->contains('buchtitel_id', $btsj->buchtitel->id) ? 1 : 0;
+            $eintrag = collect();
+            $eintrag->push($schueler);
+            $eintrag->push($buchtitel);
+
+            $liste->push($eintrag);
         }
            
         \File::delete('pdf/buecherliste.pdf');
 
-        $pdf = \PDF::loadView('pdf.buecherliste', compact('schueler', 'buchtitel'))
+        $pdf = \PDF::loadView('pdf.buecherliste', compact('liste'))
                 ->save('pdf/buecherliste.pdf');
     
         return Action::download(url('pdf/buecherliste.pdf'), 'buecherliste.pdf');
--- resources/views/pdf/buecherliste.blade.php
+++ resources/views/pdf/buecherliste.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Bestellliste</title>
+    <title>Bücherliste</title>
     
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,44 +28,60 @@
         font: normal 14px/1.5em 'PT Sans', Sans-serif;
       }
 
+      .pb_before {
+           page-break-before: always !important;
+        }
+
+      .pb_after {
+           page-break-after: always !important;
+      }
+      
     </style>
 
   </head>
 
   <body>
 
-     <h1>Bücherliste: {{ $schueler->nachname }} , {{ $schueler->vorname }} ({{ $schueler->klasse->bezeichnung }})</h1>
+    @foreach($liste as $eintrag)
 
-    <table cellpadding="3">
-      <tr>
-          <th>ausgeliehen?</th>
-          <th>Fach</th> 
-          <th>Titel</th>
-          <th>Bestellstatus</th>
-      </tr>
+      <div class="pb_before pb_after">
 
-      @foreach ($buchtitel->sortBy('wahl') as $bt)
-      <tr>                                    
-          <td>
-              @if ($bt->ausgeliehen) ja @endif 
-          </td>
-          <td>{{ $bt->buchtitel->fach->code }}</td>
-          <td>{{ $bt->buchtitel->titel }}</td>
-          <td>  
-              @if($bt->wahl==1)  
-                  leihen
-              @elseif($bt->wahl==2)
-                  verlängern
-              @elseif($bt->wahl==3)
-                  kaufen
-              @else($bt->wahl==4)
-                  nicht gewählt
-              @endif
-          </td>
-      </tr>
-      @endforeach
-    
-    </table>
+        <h1>Bücherliste: {{ $eintrag->schueler->nachname }} , {{ $eintrag->schueler->vorname }} ({{ $eintrag->schueler->klasse->bezeichnung }})</h1>
+
+        <table cellpadding="3">
+          <tr>
+              <th>ausgeliehen?</th>
+              <th>Fach</th> 
+              <th>Titel</th>
+              <th>Bestellstatus</th>
+          </tr>
+
+          @foreach ($eintrag->buchtitel->sortBy('wahl') as $bt)
+          <tr>                                    
+              <td>
+                  @if ($bt->ausgeliehen) ja @endif 
+              </td>
+              <td>{{ $bt->buchtitel->fach->code }}</td>
+              <td>{{ $bt->buchtitel->titel }}</td>
+              <td>  
+                  @if($bt->wahl==1)  
+                      leihen
+                  @elseif($bt->wahl==2)
+                      verlängern
+                  @elseif($bt->wahl==3)
+                      kaufen
+                  @else($bt->wahl==4)
+                      nicht gewählt
+                  @endif
+              </td>
+          </tr>
+          @endforeach
+        
+        </table>
+
+      </div>
+
+    @endforeach
 
   </body>
 </html>
