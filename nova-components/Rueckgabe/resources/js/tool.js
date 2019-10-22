039f9fb98e4a9105f1aa6e1a841e2baf46229e31
mblanke70 <blanke@ursulaschule.de>
2019-10-22 16:24:13 +0200

    ...

--- nova-components/Rueckgabe/resources/js/tool.js
+++
@@ -1,9 +0,0 @@
-Nova.booting((Vue, router, store) => {
-    router.addRoutes([
-        {
-            name: 'rueckgabe',
-            path: '/rueckgabe',
-            component: require('./components/Tool'),
-        },
-    ])
-})
