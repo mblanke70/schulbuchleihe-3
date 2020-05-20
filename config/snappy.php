554128a80ccc3d13dcb5994ac9415655be2fe1e1
mblanke70 <blanke@ursulaschule.de>
2020-05-20 11:48:11 +0200

    ...

--- config/snappy.php
+++ config/snappy.php
@@ -4,8 +4,8 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        //'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
-        'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
+        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        //'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
