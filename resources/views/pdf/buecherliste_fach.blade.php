494f255d565635045b1c6b8caee1f8420ad836e7
mblanke70 <blanke@ursulaschule.de>
2020-05-20 09:22:51 +0200

    ...

--- resources/views/pdf/buecherliste_fach.blade.php
+++ resources/views/pdf/buecherliste_fach.blade.php
@@ -42,51 +42,53 @@
 
   <body>
 
-    {{ $plucked }}
+    @foreach($plucked as $fach)
+   
+      <div class="pb_before pb_after">
 
-    <div class="pb_before pb_after">
-
-      <table cellpadding="3">
-
-        <tr>
-            <th>Titel</th>
-            <th>Schuljahr</th>
-            <th>ISBN</th>
-            <th>Fach</th> 
-            <th>Leihpreis</th>
-            <th>Kaufpreis</th>
-            <th>Jahrgänge</th>
-        </tr>
-
-  
-        @foreach($models as $btsj)
+        <table cellpadding="3">
 
           <tr>
-              <td>{{ $btsj->buchtitel->titel }}</td>
-              <td>{{ $btsj->schuljahr->schuljahr }}</td>
-              <td>{{ $btsj->buchtitel->isbn }}</td>
-              <td>{{ $btsj->buchtitel->fach->name }}</td>
-              <td>{{ $btsj->leihpreis }}</td>
-              <td>{{ $btsj->kaufpreis }}</td>
+              <th>Titel</th>
+              <th>Schuljahr</th>
+              <th>ISBN</th>
+              <th>Fach</th> 
+              <th>Leihpreis</th>
+              <th>Kaufpreis</th>
+              <th>Jahrgänge</th>
+          </tr>
 
-              <td>
+    
+          @foreach($fach as $btsj)
 
-                @foreach ($btsj->jahrgaenge as $jg)
+            <tr>
+                <td>{{ $btsj->buchtitel->titel }}</td>
+                <td>{{ $btsj->schuljahr->schuljahr }}</td>
+                <td>{{ $btsj->buchtitel->isbn }}</td>
+                <td>{{ $btsj->buchtitel->fach->name }}</td>
+                <td>{{ $btsj->leihpreis }}</td>
+                <td>{{ $btsj->kaufpreis }}</td>
 
-                  {{ $jg->jahrgangsstufe . " " }}
+                <td>
 
-                @endforeach                                 
-           
-              </td>
-            
-        </tr>
-        
-        @endforeach
+                  @foreach ($btsj->jahrgaenge as $jg)
 
-           
-      </table>
+                    {{ $jg->jahrgangsstufe . " " }}
 
-    </div>
+                  @endforeach                                 
+             
+                </td>
+              
+          </tr>
+          
+          @endforeach
+
+             
+        </table>
+
+      </div>
+
+    @endforeach
 
   </body>
 
