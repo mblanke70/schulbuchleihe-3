afe81444d4d33d35e4090edb3fb5c44d5111f3c3
mblanke70 <blanke@ursulaschule.de>
2020-06-10 22:55:02 +0200

    ...

--- resources/views/user/anmeldung/schritt6.blade.php
+++ resources/views/user/anmeldung/schritt6.blade.php
@@ -23,8 +23,6 @@
 
     <p>versendet worden.</p>
 
-	<p>Die Seite ist über das IServ-Menü permanent erreichbar. Schüler können hier nicht nur am Leihverfahren teilnehmen sondern später auch jederzeit einsehen, welche Bücher sie aktuell ausgeliehen haben.</p>
-
     <h5>Erm&auml;&szlig;igung auf den Leihpreis</h5>
         	
 	<p>Familien mit drei oder mehr schulpflichtigen Kindern bezahlen für jedes Kind nur 80 Prozent des Entgelts für die Ausleihe. Der Nachweis über jedes schulpflichtige Geschwisterkind, das nicht an der Ursulaschule ist, erfolgt durch Abgabe einer Schulbescheinigung. Diese können zu Beginn des nächsten Schuljahres an an dieser Stelle per Upload übermittelt oder im Sekretariat abgegeben werden.</p>
