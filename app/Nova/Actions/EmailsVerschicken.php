e9fa2e8919e8db4be0e2cf272294b81ee3a06baa
mblanke70 <blanke@ursulaschule.de>
2020-09-01 22:08:32 +0200

    ...

---
+++ app/Nova/Actions/EmailsVerschicken.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+
+use Illuminate\Support\Facades\Mail;
+
+use App\Mail\Schulbescheinigung;
+
+class EmailsVerschicken extends Action
+{
+    use InteractsWithQueue, Queueable;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        foreach($models as $familie) 
+        {
+            $mail = new Schulbescheinigung($familie);
+            Mail::to($familie->email)->send($mail);
+        }
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return [];
+    }
+}
---
+++ resources/views/emails/orders/certificate.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+# Liebe Eltern,
+
+wir möchten Sie noch einmal daran erinnern, dass Familien mit drei oder mehr schulpflichtigen Kindern das Recht auf einen Nachlass der Gebühren für die geliehenen Schulbücher um 20 % haben.
+
+Sollten Sie einen Anspruch auf diesen Nachlass haben, so bitten wir Sie, zu überprüfen, ob Sie eine Schulbescheinigung bei uns eingereicht haben. Nur mit den aktuellen Schulbescheinigungen der Geschwisterkinder können wir diesen Nachlass gewähren.
+
+(Sind die Geschwisterkinder an der Ursulaschule, so benötigen wir KEINE Bescheinigungen. Unsere eigenen Schüler und Schülerinnen kennen wir!)
+
+Noch fehlende Schulbescheinigungen können Sie bis zum Ende dieser Woche im Sekretariat nachreichen.
+
+Mit freundlichen Grüßen<br>
+M. Hoffmann, M. Blanke, K. Schwegmann
+
+SchulBuchLeihe
+@endcomponent
