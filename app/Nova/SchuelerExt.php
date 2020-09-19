a559c763ac8c5b6a914239be4c7d241063497618
mblanke70 <blanke@ursulaschule.de>
2020-09-19 11:15:49 +0200

    ...

--- app/Nova/SchuelerExt.php
+++ app/Nova/SchuelerExt.php
@@ -122,6 +122,8 @@ class SchuelerExt extends Resource
      */
     public function actions(Request $request)
     {
-        return [];
+        return [
+            (new Actions\SchuelerExtBestaetigen)->showOnTableRow(),
+        ];
     }
 }
