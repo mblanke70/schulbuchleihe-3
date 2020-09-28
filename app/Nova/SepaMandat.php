0321e7d214a8ee7493ae284ee62681d52b0a967e
mblanke70 <blanke@ursulaschule.de>
2020-09-28 22:46:35 +0200

    ...

--- app/Nova/SepaMandat.php
+++ app/Nova/SepaMandat.php
@@ -76,15 +76,15 @@ class SepaMandat extends Resource
     {
         return [
 
-            Text::make('Mandatsreferenz', 'debtorMandate')->rules('required')->sortable(),
+            Text::make('Mandatsreferenz', 'debtorMandate')->sortable(),
 
-            Date::make('Signaturdatum', 'debtorMandateSignDate')->rules('required')->sortable(),
+            Date::make('Signaturdatum', 'debtorMandateSignDate')->sortable(),
 
-            Text::make(' Name', 'debtorName')->rules('required'),
+            Text::make(' Name', 'debtorName'),
 
-            Text::make('BIC', 'debtorBic')->rules('required'),
+            Text::make('BIC', 'debtorBic'),
 
-            Text::make('IBAN', 'debtorIban')->rules('required'),
+            Text::make('IBAN', 'debtorIban'),
 
             //Text::make('Gläubiger Name', 'creditorName')->rules('required')->sortable(),
 
